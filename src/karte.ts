51a7ef4bb3cd373c23501b3fa2a842774a815f22
Lena Höding <l.hoeding@tu-braunschweig.de>
2023-07-25 11:09:27 +0200

    NPC1 dialog

--- src/karte.ts
+++ src/karte.ts
@@ -12,12 +12,10 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     
     //code here
-    console.log('test1');
     showScore();
     clock();
 
-    //old/remove
-    scoreAdd(3);
+    
     ncp1();
 
     //variables
@@ -39,7 +37,7 @@ function clock() {
     currentPopup = WA.ui.openPopup("popupClock", "Es ist " + time, []);
   })
 
-  WA.room.area.onLeave('zoneClock').subscribe(closePopup)
+  WA.room.area.onLeave('zoneClock').subscribe(closePopup);
 }
 
 function showScore() {
@@ -48,82 +46,130 @@ function showScore() {
     //console.log(WA.player.name, ' entered score zone');
     currentPopup = WA.ui.openPopup("popupScore", "Score: " + WA.player.state.score, []);
   })
+
+  //closes popup when leaving zone
+  WA.room.area.onLeave('zoneScore').subscribe(closePopup);
 }
 
 function ncp1() {
+  let hasBeenSpokenTo = false;
+  let popClosedHand = false;
+
   WA.room.area.onEnter('npc1').subscribe(() => {
-    
     console.log("entered npc1 zone");
 
-
-    currentPopup = WA.ui.openPopup("popup1", 'Willkommen im Hospital, wie kann ich Ihnen weiterhelfen?', [{
-      label: "Hallo, mein Name ist " + WA.player.name + ". Ich bin für die Prozessanalyse hier.",
+    if (!hasBeenSpokenTo) {
+      //start dialoging
+      currentPopup = WA.ui.openPopup("popup1", 'Willkommen im Hospital, wie kann ich Ihnen weiterhelfen?', [{
+        label: "Hallo, mein Name ist " + WA.player.name + ". Ich bin für die Prozessanalyse hier.",
         className: "primary",
         callback: (currentPopup) => {
-            // Close the popup when the "Close" button is pressed.
-            currentPopup.close();
+          // Close the popup when the "Close" button is pressed.
+          currentPopup.close();
 
-            dialogChooseRole();
+          dialogChooseRole();
+          hasBeenSpokenTo = true;
+          }
+      }]);
+    } else {
+      currentPopup = WA.ui.openPopup("popup1", 'Nutzen Sie die Meetingräume, um sich mit anderen auszutauschen!', [{
+        label: "Okay",
+        className: "primary",
+        callback: (currentPopup) => {
+          // Close the popup when the "Close" button is pressed.
+          popClosedHand = true;
+          currentPopup.close();
+          }
+      }]);
+
+      WA.room.area.onLeave('npc1').subscribe(() => {
+        if (hasBeenSpokenTo && !popClosedHand) {
+          currentPopup.close();
         }
-    }]);
-
-    
+      })
+    }
   })
-  
 
+  
+  
 }
 
 function dialogLeftWay() {
-//toDo
+  let directions = "";
+
+  if (WA.player.state.role == "med") {
+    directions = "Den Hauptgang entlang, dann zweimal links. Wahrscheinlich finden Sie jemanden im Pausenraum.";
+    
+  } else if (WA.player.state.role == "inf") {
+    directions="Den Hauptgang entlang, dann links. Als nächstes rechts und Sie sind bei den Büros.";
+  } else {
+    console.log("Error: no role choosen");
+  }
+
+  currentPopup = WA.ui.openPopup("popup1", directions, [{
+    label: "Danke",
+    className: "primary",
+    callback: (currentPopup) => {
+      // Close the popup when the "Close" button is pressed.
+      currentPopup.close();
+    }
+  }]);
 }
 
 function dialogRightWay() {
-  //toDo
+  currentPopup = WA.ui.openPopup("popup1", 'Da hätten wir die Ärzt:innen, die IT-Techniker:innen und natürlich unsere Schreibkräfte. Deren Büros finden Sie die Hauptgang entlang und dann rechts.', [{
+    label: "Danke",
+    className: "primary",
+    callback: (currentPopup) => {
+      // Close the popup when the "Close" button is pressed.
+      currentPopup.close();
+    }
+  }]);
 }
 
 function dialogAskWay() {
   currentPopup = WA.ui.openPopup("popup1", 'Wie aufregend!', [{
     label: "Wo finde ich meine Kollegen?",
-      className: "primary",
-      callback: (currentPopup) => {
-                // Close the popup when the "Close" button is pressed.
-          currentPopup.close();
-          dialogLeftWay();
-      }
+    className: "primary",
+    callback: (currentPopup) => {
+      // Close the popup when the "Close" button is pressed.
+      currentPopup.close();
+      dialogLeftWay();
+    }
   },
   {
     label: "Wer ist alles am Schreiben eines Arztbriefs beteiligt?",
-      className: "primary",
-      callback: (currentPopup) => {
-          // Close the popup when the "Close" button is pressed.
-          currentPopup.close();
-          dialogRightWay();
-      }
+    className: "primary",
+    callback: (currentPopup) => {
+      // Close the popup when the "Close" button is pressed.
+      currentPopup.close();
+      dialogRightWay();
+    }
   }]);
 }
 
 function dialogChooseRole() {
   currentPopup = WA.ui.openPopup("popup1", 'Sehr schön. In welchem Bereich arbeiten Sie?', [{
     label: "Informatik",
-      className: "primary",
-      callback: (currentPopup) => {
-        setRole("inf");
+    className: "primary",
+    callback: (currentPopup) => {
+      setRole("inf");
 
-          // Close the popup when the "Close" button is pressed.
-          currentPopup.close();
-          dialogAskWay();
-      }
+      // Close the popup when the "Close" button is pressed.
+      currentPopup.close();
+      dialogAskWay();
+    }
   },
   {
     label: "Medizin",
-      className: "primary",
-      callback: (currentPopup) => {
-        setRole("med");
+    className: "primary",
+    callback: (currentPopup) => {
+      setRole("med");
 
-          // Close the popup when the "Close" button is pressed.
-          currentPopup.close();
-          dialogAskWay();
-      }
+      // Close the popup when the "Close" button is pressed.
+      currentPopup.close();
+      dialogAskWay();
+    }
   }]);
 }
 
@@ -132,19 +178,19 @@ function copyspeicher() {
 
   currentPopup = WA.ui.openPopup("popup1", 'Willkommen im Hospital, wie kann ich Ihnen weiterhelfen?', [{
     label: "Hallo, mein Name ist " + WA.player.name + ". ich bin wegen der Prozessanalyse hier.",
-      className: "primary",
-      callback: (currentPopup) => {
-          // Close the popup when the "Close" button is pressed.
-          currentPopup.close();
-      }
+    className: "primary",
+    callback: (currentPopup) => {
+      // Close the popup when the "Close" button is pressed.
+      currentPopup.close();
+    }
   },
   {
     label: "Nope",
-      className: "primary",
-      callback: (currentPopup) => {
-          // Close the popup when the "Close" button is pressed.
-          currentPopup.close();
-      }
+    className: "primary",
+    callback: (currentPopup) => {
+      // Close the popup when the "Close" button is pressed.
+      currentPopup.close();
+    }
   }]);
 }
 
@@ -163,6 +209,7 @@ function setRole(newRole: string) {
   } else {
     console.log("Error: WA.player.state.role is not a number.");
   }
+  scoreAdd(50);
 }
 
 function scoreAdd(numberOfPoints: number) {
